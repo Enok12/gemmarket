e7caa78aff0c2710cbffdee15343b119e10bb1a9
Enok Dilshan <73767944+Enok12@users.noreply.github.com>
2026-05-16 20:39:14 +0700

    add native offline handling

--- android/app/src/main/java/com/ggmp/marketplace/MainActivity.java
+++ android/app/src/main/java/com/ggmp/marketplace/MainActivity.java
@@ -1,5 +1,41 @@
 package com.ggmp.marketplace;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.content.Context;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    private static final String APP_URL = "https://gemmarket.vercel.app";
+    private static final String OFFLINE_URL = "file:///android_asset/public/offline.html";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        checkAndLoad();
+    }
+
+    private void checkAndLoad() {
+        if (isConnected()) {
+            getBridge().getWebView().loadUrl(APP_URL);
+        } else {
+            getBridge().getWebView().loadUrl(OFFLINE_URL);
+        }
+    }
+
+    private boolean isConnected() {
+        ConnectivityManager cm = (ConnectivityManager)
+            getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+    }
+}
