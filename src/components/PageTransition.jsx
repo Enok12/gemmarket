5b664f4369e25ba964e5d4f41350714a34555e92
Enok Dilshan <73767944+Enok12@users.noreply.github.com>
2026-08-02 13:39:56 +0530

    Caching

--- src/components/PageTransition.jsx
+++ src/components/PageTransition.jsx
@@ -10,12 +10,11 @@ export default function PageTransition({ children }) {
     <AnimatePresence mode="wait">
       <motion.div
         key={pathname}
-        initial={{ opacity: 0, y: 8 }}
-        animate={{ opacity: 1, y: 0 }}
-        // exit={{ opacity: 0, y: -8 }}
+        initial={{ y: 8 }}
+        animate={{ y: 0 }}
         transition={{
-          duration: 0.6,
-          ease: 'easeInOut',
+          duration: 0.3,
+          ease: 'easeOut',
         }}
       >
         {children}
